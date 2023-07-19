18f72ee6975c104c4992b585728963bbadba25ea
Waznygx <135837095+Waznygx@users.noreply.github.com>
2023-07-19 17:17:32 +0800

    Add files via upload

--- test2.c
+++ test2.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<math.h>
+#include<stdlib.h>
 //int main()
 //{
 //	char arr[] = "abcdef";
@@ -134,64 +135,187 @@
 //	return 0;
 //}
 
-////输出菱形图案
-////      *
-////     ***
-////    *****
-////   *******
-////  *********
-//// ***********
-////*************   
-//// ***********
-////  *********
-////   *******
-////    *****
-////     ***
-////      *
-void print_down(int line_half)
+//输出菱形图案
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+//*************   
+// ***********
+//  *********
+//   *******
+//    *****
+//     ***
+//      *
+//void print_down(int line_half)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < line_half - 1; i++)
+//	{
+//		//打印空格
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf(" ");
+//		}
+//		//打印 *
+//		for (j = 0; j < 2 * (line_half -1 - i) - 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+//void print_up(int line_half)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < line_half; i++)
+//	{
+//		//打印空格
+//		for (j = 0; j < line_half - 1 - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		//打印 *
+//		for (j = 0; j < 2 * i + 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int line_half = 0;
+//	printf("输入上半部分的行数(奇数):");
+//	scanf("%d", &line_half);
+//	print_up(line_half);
+//	print_down(line_half);
+//	return 0;
+//}
+
+////喝汽水问题
+////1瓶汽水1元。2个空瓶可以换1瓶汽水，给20元，可以买多少汽水
+//int main()
+//{
+//	int money = 0;
+//	int soda = 0;
+//	printf("input:");
+//	scanf("%d" ,&money);
+//	if (money < 0)
+//	{
+//		printf("soda=0\n");
+//	}
+//	else
+//	{
+//		printf("soda=%d\n", 2 * money - 1);
+//	}
+//	return 0;
+//}
+////int main()
+////{
+////	int money = 0;
+////	int soda = 0;
+////	int soda1 = 0;
+////	int bottle = 0;
+////	for ( money = 1; money <= 20; money++)
+////	{
+////		soda++;
+////		bottle++;
+////	}
+////	while (soda1 = bottle / 2)
+////	{
+////		bottle = soda1 + bottle % 2;
+////		soda += soda1;
+////	}
+////	printf("%d\n", soda);
+////	return 0;
+////}
+
+//调整奇数偶数顺序，使数组奇数放在偶数之前
+//void print_arr(int arr[], int length)
+//{
+//	int i = 0;
+//	for (i = 0; i < length; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+//void swap(int arr[], int length)
+//{
+//	int left = 0;
+//	int right = length - 1;
+//	while (left < right)
+//	{
+//		//
+//		while ((left < right) && (arr[left] % 2 == 1))
+//		{
+//			left++;
+//		}
+//		//
+//		while ((left < right) && (arr[right] % 2 == 0))
+//		{
+//			right--;
+//		}
+//		//交换奇偶数
+//		if (left < right)
+//		{
+//			int temp = arr[left];
+//			arr[left] = arr[right];
+//			arr[right] = temp;
+//		}
+//	}
+//}
+// int main()
+//{
+//	int arr[] = { 2,54,3,5,1,46,0,34,72,5,43,11,63 };
+//	int length = sizeof(arr) / sizeof(arr[0]); 
+//	swap(arr, length);
+//	print_arr(arr, length);
+//	/*int i = 0;
+//	for ( i = 0; i < length; i++)
+//	{
+//		if (arr[i] % 2 == 1)
+//		{
+//			printf("%d ", arr[i]);
+//		}
+//	}
+//	for (i = 0; i < length; i++)
+//	{
+//		if (arr[i] % 2 == 0)
+//		{
+//			printf("%d ", arr[i]);
+//		}
+//	}*/
+//	return 0;
+//}
+
+//实现一个函数，可以做选字符串中k个字符
+//例如：
+//ABCD左旋一个字符得到BCDA
+//ABCD右旋一个字符得到CDBA
+void turn_left(char arr[], int k,int len)
 {
-	int i = 0;
-	int j = 0;
-	for (i = 0; i < line_half - 1; i++)
+	char temp = arr[0];
+	arr[0] = arr[1];
+	if (k >= 1)
 	{
-		//打印空格
-		for (j = 0; j <= i; j++)
-		{
-			printf(" ");
-		}
-		//打印 *
-		for (j = 0; j < 2 * (line_half -1 - i) - 1; j++)
-		{
-			printf("*");
-		}
-		printf("\n");
+		k--;
+		turn_left(arr + 1, k, len);
 	}
-}
-void print_up(int line_half)
-{
-	int i = 0;
-	int j = 0;
-	for (i = 0; i < line_half; i++)
-	{
-		//打印空格
-		for (j = 0; j < line_half - 1 - i; j++)
-		{
-			printf(" ");
-		}
-		//打印 *
-		for (j = 0; j < 2 * i + 1; j++)
-		{
-			printf("*");
-		}
-		printf("\n");
-	}
+	arr[len - 1] = arr[0];
 }
 int main()
 {
-	int line_half = 0;
-	printf("输入上半部分的行数(奇数):");
-	scanf("%d", &line_half);
-	print_up(line_half);
-	print_down(line_half);
+	char arr[] = "";
+	int k = 1;
+	int len = strlen(arr);
+	printf("请输入字符和要旋转的数字:");
+	gets(arr);
+	turn_left(arr, k,len);
+	printf("%s ", arr);
 	return 0;
 }
